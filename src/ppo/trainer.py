f35bc76250c8d375e4c78e900840d3ff8cd700ec
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-09-03 22:03:22 +0200

    Initialize variables in trainer

--- src/ppo/trainer.py
+++ src/ppo/trainer.py
@@ -10,28 +10,36 @@ from torch.utils.tensorboard import SummaryWriter
 
 from src.hparams import HParams
 from src.utils import LOG_INFO, make_env
+from src.ppo.agent import Agent
 
 
-class PPOAgent:
+class Trainer:
     """Trainer for proximal policy optimization."""
-    def __init__(self, hparams: HParams):
+
+    def __init__(self, hparams: HParams, device: torch.device):
         """Initialize the trainer."""
         self.hparams = hparams
+        self.device = device
         self.envs = gym.vector.SyncVectorEnv(
-            [
-                make_env(
-                    hparams.gym_env,
-                    hparams.seed,
-                    i,
-                    hparams.capture_video,
-                    hparams.run_name,
-                )
-                for i in range(hparams.num_envs)
-            ]
+            [make_env(i, hparams) for i in range(hparams.num_envs)]
         )
         assert isinstance(
             self.envs.single_action_space, gym.spaces.Discrete
         ), "only discrete action space is supported"
+
+        # Init variables
+        self.global_step = 0
+        self.next_obs = torch.Tensor(self.envs.reset()).to(self.device)
+        self.next_done = torch.zeros(hparams.num_envs).to(self.device)
+
+        # Init model
+        self.agent = Agent(self.envs).to(self.device)
+        self.optimizer = optim.Adam(
+            self.agent.parameters(), lr=hparams.lr, eps=hparams.eps
+        )
+
+        # Init tensorboard
+        self.writer = SummaryWriter(log_dir=f"{hparams.log_dir}/{hparams.run_name}")
 
     def train(self):
         """Train the model."""
