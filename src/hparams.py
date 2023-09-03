eb2b1d68f026fb1aece1dff5409f4227324dd5af
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-09-03 18:48:21 +0200

    Add load_params function

--- src/hparams.py
+++ src/hparams.py
@@ -1,11 +1,21 @@
 """Hyperparameters for training and testing."""
-import os
 from dataclasses import dataclass, field
+from pathlib import Path
 
+import yaml
 
 @dataclass
 class HParams:
-    experiment_name: str = field(default="blokus", type=str, metadata={"help": "Name of the experiment"})
-    seed: int = field(default=42, type=int, metadata={"help": "Seed for the experiment"})
-    num_envs: int = field(default=4, type=int, metadata={"help": "Number of parallel environments"})
-    gym_env: str = field(default="blokus_gym:blokus-simple-v0", type=str, metadata={"help": "Gym environment ID"})
+    experiment_name: str = field(default="blokus", metadata={"help": "Name of the experiment"})
+    seed: int = field(default=42, metadata={"help": "Seed for the experiment"})
+    num_envs: int = field(default=4, metadata={"help": "Number of parallel environments"})
+    gym_env: str = field(default="blokus_gym:blokus-simple-v0", metadata={"help": "Gym environment ID"})
+
+
+def load_params(hparam_fp: Path | None = None) -> HParams:
+    """Load hyperparameters from a YAML file."""
+    if hparam_fp is None:
+        return HParams()
+    with open(hparam_fp, "r", encoding="utf-8") as f:
+        hparams_dict = yaml.safe_load(f)
+    return HParams(**hparams_dict)
