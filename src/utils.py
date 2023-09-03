6aed2e28cb0848fb6ee5b9d6844eff3be54831af
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-09-03 20:54:41 +0200

    Add seed fn

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 """Utility functions for the project."""
+import random
 import logging
 
+import torch
+import numpy as np
 import coloredlogs
 
 # Set up logging
@@ -13,10 +16,24 @@ handler.setFormatter(
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
+
 def LOG_INFO(msg: str):
     """Log an info message."""
     logger.info(msg)
 
+
 def LOG_WARNING(msg: str):
     """Log a warning message."""
     logger.warning(msg)
+
+
+def LOG_ERROR(msg: str):
+    """Log an error message."""
+    logger.error(msg)
+
+
+def seed(seed_number: int):
+    random.seed(seed_number)
+    np.random.seed(seed_number)
+    torch.manual_seed(seed_number)
+    torch.backends.cudnn.deterministic = True
