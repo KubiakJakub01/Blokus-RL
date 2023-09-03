433041e0d8aa2a191f448c1c581877d54fdcbdbd
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-09-03 18:36:06 +0200

    Add basic params

--- src/hparams.py
+++ src/hparams.py
@@ -1,8 +1,11 @@
 """Hyperparameters for training and testing."""
 import os
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class HParams:
-    pass
+    experiment_name: str = field(default="blokus", type=str, metadata={"help": "Name of the experiment"})
+    seed: int = field(default=42, type=int, metadata={"help": "Seed for the experiment"})
+    num_envs: int = field(default=4, type=int, metadata={"help": "Number of parallel environments"})
+    gym_env: str = field(default="blokus_gym:blokus-simple-v0", type=str, metadata={"help": "Gym environment ID"})
