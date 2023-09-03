aadea40465f23f20c4702d01b6173da0d3b05b68
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-09-03 21:32:07 +0200

    Create Critic network

--- src/ppo/agent.py
+++ src/ppo/agent.py
@@ -11,3 +11,14 @@ def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
     torch.nn.init.constant_(layer.bias, bias_const)
     return layer
 
+
+class Critic(nn.Module):
+    def __init__(self, envs):
+        super(Critic, self).__init__()
+        self.critic = nn.Sequential(
+            layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 64)),
+            nn.Tanh(),
+            layer_init(nn.Linear(64, 64)),
+            nn.Tanh(),
+            layer_init(nn.Linear(64, 1), std=1.0),
+        )
