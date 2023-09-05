e9e35ef305df9c11087270d11b3136ff8e491052
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-09-05 21:37:19 +0200

    Add _compute_anneal_lr function

--- src/ppo/trainer.py
+++ src/ppo/trainer.py
@@ -11,6 +11,7 @@ from torch.utils.tensorboard import SummaryWriter
 from src.hparams import HParams
 from src.utils import LOG_INFO, make_env
 from src.ppo.agent import Agent
+from src.ppo.memory import Memory
 
 
 class Trainer:
@@ -32,14 +33,31 @@ class Trainer:
         self.next_obs = torch.Tensor(self.envs.reset()).to(self.device)
         self.next_done = torch.zeros(hparams.num_envs).to(self.device)
 
-        # Init model
+        # Init model, optimizer and memory
         self.agent = Agent(self.envs).to(self.device)
         self.optimizer = optim.Adam(
             self.agent.parameters(), lr=hparams.lr, eps=hparams.eps
         )
+        self.memory = Memory(hparams, self.envs, self.device)
 
         # Init tensorboard
         self.writer = SummaryWriter(log_dir=f"{hparams.log_dir}/{hparams.run_name}")
 
     def train(self):
         """Train the model."""
+        for update in range(1, self.hparams.num_updates + 1):
+             # Annealing the rate if instructed to do so.
+            if self.hparams.anneal_lr:
+                self.optimizer.param_groups[0]["lr"] = self._compute_anneal_lr(update)
+
+
+    def _compute_anneal_lr(self, update: int) -> float:
+        """Compute the annealed learning rate.
+        
+        Args:
+            update: Current update step.
+        
+        Returns:
+            Annealed learning rate."""
+        frac = 1.0 - (update - 1.0) / self.hparams.num_updates
+        return frac * self.hparams.learning_rate
